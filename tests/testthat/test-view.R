297aba788690b671bf5eb86fbbb6d6d4c6ab5991
Charles T. Gray <charlestigray@gmail.com>
2021-03-10 19:19:16 +0000

    another test

--- tests/testthat/test-view.R
+++ tests/testthat/test-view.R
@@ -5,3 +5,7 @@ test_that("neet", {
 test_that("distill option", {
   expect_s3_class(pandan_view(distill = TRUE), "ggplot")
 })
+
+test_that("single project view", {
+  expect_s3_class(pandan_view("hpp"), "ggplot")
+})
