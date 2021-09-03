a617437c9210e10533415f5756dc7abd49e47c99
Charles T. Gray <charlestigray@gmail.com>
2021-09-03 21:59:50 +0100

    switching to lappy386

--- R/pandan_gantt.R
+++ R/pandan_gantt.R
@@ -43,20 +43,78 @@ wrangle_gantt_dat <- function(
 
 pandan_gantt <- function() {
 
-  gantt_plot <-
-  wrangle_gantt_dat() %>%
+  gantt_dat <-
+    wrangle_gantt_dat()
+
+  events <-
+    googlesheets4::read_sheet(Sys.getenv("PANDAN_GANTT"), "events") %>%
+    dplyr::mutate(
+      date = lubridate::ymd(date)
+    ) %>%
+    dplyr::bind_rows(
+      tibble::tibble(
+      date = lubridate::today(),
+      event = "today")
+    ) %>%
+    dplyr::mutate(
+      id = dplyr::row_number()
+    ) %>%
+    dplyr::arrange(desc(id)) %>%
+    dplyr::select(-id)
+
+  #gantt_plot <-
+    gantt_dat %>%
+    dplyr::mutate(
+      month = lubridate::month(start, label = TRUE, abbr = FALSE)
+    ) %>%
     ggplot2::ggplot() +
+    ggplot2::geom_vline(
+      data = events,
+      ggplot2::aes(
+        xintercept = date,
+        linetype = event
+      ),
+      alpha = 0.4
+    ) +
     ggplot2::geom_segment(
       ggplot2::aes(
         x = start,
         xend = end,
         y = project,
         yend = project
-      )
-    )
+      ),
+      size = 10,
+      alpha = 0.3
+    ) +
+    ggplot2::geom_text(
+      ggplot2::aes(
+        x = start,
+        y = project,
+        label = stringr::str_wrap(objective, 20)
+      ),
+      size = 3.5,
+      nudge_y = 0.3,
+      hjust = 0,
+      alpha = 0.8
+    ) +
+    ggthemes::theme_solarized_2() +
+    ggplot2::labs(
+      title = "Stuff Charles is working on",
+      x = "date"
+    ) +
+      ggplot2::theme(
+        legend.position = "bottom"
+      ) +
+      ggplot2::facet_wrap(~ month, scales = "free")
 
-      plotly::ggplotly(p = gantt_plot,
-                       tooltip = c("objective", "priorities"))
+  # gantt_gt <-
+  #   gantt_dat %>%
+  #   dplyr::select(project, objective)
+
+
+
+      # plotly::ggplotly(p = gantt_plot,
+      #                  tooltip = c("objective", "priorities"))
 
 
 }
